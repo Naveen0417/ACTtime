98cb0482cff7b4024adf772dacb15a9e5efd61a9
Naveen0417 <naveenkumarreddy106@gmail.com>
2021-01-19 15:45:31 +0530

    addded few more files

--- selenium/src/main/java/com/actTime/properties/sample.java
+++
@@ -1,5 +0,0 @@
-package com.actTime.properties;
-
-public class sample {
-
-}
--- selenium/src/test/java/com/actTime/base/base.java
+++ selenium/src/test/java/com/actTime/base/base.java
@@ -1,47 +1,92 @@
+/**Automation for ACTtime |
+ * Module : Base Class for ACTtime
+ * Jan 19, 2021
+ */
 package com.actTime.base;
 
+/**
+ * @author Naveen G
+ *
+ */
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class base {
 
-	public static void main(String[] args) {
+	public static final String DATA_PROPERTIES = "data.properties";
 
-		// new method for declaring browser
-		WebDriver driver;
-		WebDriverManager.chromedriver().setup();
-		driver = (new ChromeDriver());
+	private WebDriver driver;
+	private Properties Prop;
 
-		// old method for declaring browser
-//		System.setProperty("webdriver.chrome.driver", "E://cdriver//chromedriver.exe");
-//		ChromeDriver driver = new ChromeDriver();
+	public WebDriver getDriver() {
+		return driver;
+	}
 
-		driver.get("http://127.0.0.1:81/login.do");
+	public void setDriver(String browser) {
+		if (browser.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = (new ChromeDriver());
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = (new FirefoxDriver());
+		} else if (browser.equalsIgnoreCase("ie")) {
+			WebDriverManager.iedriver().setup();
+			driver = (new InternetExplorerDriver());
+		}
+	}
+
+	public Properties getProp() {
+		Prop = new Properties();
+		return Prop;
+	}
+
+	@BeforeTest
+	public void openBrowser() throws IOException {
+		InputStream datapropertieStream = getClass().getClassLoader().getResourceAsStream(DATA_PROPERTIES);
+		if (datapropertieStream != null) {
+			getProp().load(datapropertieStream);
+		}
+		setDriver(Prop.getProperty("browser"));
+		driver.get(Prop.getProperty("url"));
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-		driver.findElement(By.name("username")).sendKeys("admin");
-		driver.findElement(By.name("pwd")).sendKeys("manager");
-		driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
-		driver.findElement(By.xpath("//a[text()=\"Users\"]")).click();
-		driver.findElement(By.xpath("//input[@type=\"button\"]")).click();
-		// Copy Text
-		WebElement copy = driver.findElement(By.name("username"));
-		copy.sendKeys("Naveen");
-		Actions builder = new Actions(driver);
-		builder.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build().perform();
-		builder.keyDown(Keys.CONTROL).sendKeys("c").keyUp(Keys.CONTROL).build().perform();
-		// Paste Text
-		WebElement Paste = driver.findElement(By.name("firstName"));
-		Paste.click();
-		builder.keyDown(Keys.CONTROL).sendKeys("v").keyUp(Keys.CONTROL).build().perform();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	}
 
+	@BeforeClass
+	public void loginPage() {
+		String username = Prop.getProperty("username");
+		String password = Prop.getProperty("password");
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("pwd")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
+		System.out.println("The logout page is" + driver.getTitle());
+	}
+
+	@AfterClass
+	public void logout() {
+		driver.findElement(By.className("logoutImg")).click();
+		System.out.println("The logout page is" + driver.getTitle());
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
 	}
 
 }
